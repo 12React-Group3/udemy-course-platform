8b4c3b35d505bb41de59de2fe291d1ab74165376
Lycopenee <309334941@qq.com>
2026-01-12 20:26:55 -0600

    feat: add Mongoose schemas for User, Course, Question, Task, and TaskRecord; set up initial route files and server structure

--- udemy-backend/server.js
+++ udemy-backend/server.js
@@ -4,6 +4,10 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT ? Number(process.env.PORT) : 5000
 
+// import routers
+
+
+
 app.use(
 	cors({
 		origin: ['http://localhost:5173'],
@@ -18,6 +22,25 @@ app.get('/api/hello', (req, res) => {
 	})
 })
 
+// use Routers as middleware
+// app.use('/api/auth',authRoutes)
+// app.use('/api/courses',courseRoutes)
+// app.use('/api/tasks',taskRoutes)
+// app.use('/api/questions',questionRoutes)
+
+
+
+// 404 route handler
+app.use((req,res)=>{
+    res.status(404).json({
+        success:false,
+        error:'Route node found',
+        statusCode:404
+    });
+});
+
+
+// start the server
 app.listen(port, () => {
 	// eslint-disable-next-line no-console
 	console.log(`Backend listening on http://localhost:${port}`)
