322ba7b814157825f8757030b7cab685e294a987
Lycopenee <309334941@qq.com>
2026-01-12 20:45:33 -0600

    rebased again feat: update course schema to include involved users and add task routes documentation

---
+++ udemy-backend/routes/taskRoutes.js
@@ -0,0 +1,10 @@
+
+
+
+// get course routes
+// get '/:taskId' - get task by id
+// post '/createTask' - create a new task, admin only(teacher only)
+// put '/:taskId' - update a task by id , admin only(teacher only)
+// delete '/:taskId' - delete a task by id, admin only(teacher only)
+// submite "/:id/:taskId"  submite homework, or quiz for a course
+// get "/:id/:taskRecordId" - get task record by id for task review
