4b159fc919123011b2dae689054b7b00cac8b26c
Lycopenee <309334941@qq.com>
2026-01-16 12:40:41 -0600

    feat: enhance login page with updated styles and messaging; remove unused Tailwind config KAN-105 login, register frontend

--- udemy-frontend/src/pages/Auth/Login.jsx
+++ udemy-frontend/src/pages/Auth/Login.jsx
@@ -25,13 +25,15 @@ const Login = () => {
     };
     return (
         <div className='min-h-screen flex items-center justify-center bg-gray-100 min-w-screen'>
-            <div className="login-container m-4 display-grid  border-2 w-full sm:w-1/2 rounded-8 p-16 box-shadow-light bg-white">
-                <h2 className="font-bold text-2xl ">Login</h2>
+            <div className="login-container m-4 display-grid  border w-full sm:max-w-lg rounded-2xl p-16 box-shadow-light bg-white l">
+                <h2 className="font-bold text-2xl ">Welcome Back</h2>
+                <h3 className="text-gray-600 mb-6">Login to your account, kick off your today learning</h3>
                 {error && <p className="error-message">{error}</p>}
                 <form onSubmit={handleSubmit}>
                     <div>
                         <label>Email:</label>
                         <input
+                          className='border rounded-lg p-2 w-full mb-4'
                             type="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
@@ -41,13 +43,16 @@ const Login = () => {
                     <div>
                         <label>Password:</label>
                         <input
+                          className='border rounded-lg p-2 w-full mb-4'
                             type="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
                         />
                     </div>
-                    <button type="submit">Login</button>
+                    <button className='border rounded-lg p-2 w-full mb-4 bg-brand! text-white' type="submit">Login</button>
+                    <h3> Forgot password?  <a href="/forgot-password" className="text-brand hover:underline">Click here</a> </h3>
+                    <h3> Don't have an account? <a href="/register" className="text-brand hover:underline">Register here</a> </h3>
                 </form>
             </div>
         </div>
